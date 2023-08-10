8ff5e80154993df4e3ddfbd4b33273e3cfcd4df0
Teerut Srithongdee <teerutsr@gmail.com>
2023-08-10 14:25:14 +0700

    [+] add navbar link success

--- src/main/java/cs211/project/controllers/myEvent/MyEventViewController.java
+++ src/main/java/cs211/project/controllers/myEvent/MyEventViewController.java
@@ -3,6 +3,7 @@ package cs211.project.controllers.myEvent;
 import cs211.project.services.FXRouter;
 import cs211.project.utils.ComponentRegister;
 import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -11,10 +12,13 @@ public class MyEventViewController extends ComponentRegister {
 
     @FXML
     private VBox SideBarVBox;
+    @FXML
+    private HBox NavBarHBox;
 
     @FXML
     public void initialize() {
         this.loadSideBarComponent(SideBarVBox, "SideBarComponent.fxml");
+        this.loadNavBarComponent(NavBarHBox, "NavBarComponent.fxml");
     }
 
     @FXML public void goToCreateEvent() {
