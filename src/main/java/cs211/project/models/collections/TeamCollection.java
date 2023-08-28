fa86139fda835cc4c90a6dafb7cd942387c8426d
JarBenjaporn <benjaporn.sukp@ku.th>
2023-08-28 23:55:57 +0700

    finish add team colloestion

--- src/main/java/cs211/project/models/collections/TeamCollection.java
+++ src/main/java/cs211/project/models/collections/TeamCollection.java
@@ -1,5 +1,5 @@
 package cs211.project.models.collections;
-
+import cs211.project.models.Team;
 import java.util.ArrayList;
 
 public class TeamCollection {
@@ -9,8 +9,30 @@ public class TeamCollection {
         this.teams = new ArrayList<>();
     }
 
-    public void add(Team teams) {
-
+    public void add(Team team) {
+        this.teams.add(team);
     }
+
+    public ArrayList<Team> getTeams() {
+        return this.teams;
+    }
+
+    public void setTeams(ArrayList<Team> teams) {
+        this.teams = teams;
+    }
+
+    public void remove(Team team) {
+        this.teams.remove(team);
+    }
+
+    public Team findByName(String name) {
+        for(Team team : this.teams) {
+            if(team.getName().equals(name)) {
+                return team;
+            }
+        }
+        return null;
+    }
+
 
 }
