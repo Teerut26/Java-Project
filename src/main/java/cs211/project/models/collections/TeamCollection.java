48b4336859c7f57f146a040ecbd90a84624c2e05
JarBenjaporn <benjaporn.sukp@ku.th>
2023-08-28 23:28:22 +0700

    not yet add team collection

---
+++ src/main/java/cs211/project/models/collections/TeamCollection.java
@@ -0,0 +1,16 @@
+package cs211.project.models.collections;
+
+import java.util.ArrayList;
+
+public class TeamCollection {
+    private ArrayList<Team> teams;
+
+    public TeamCollection() {
+        this.teams = new ArrayList<>();
+    }
+
+    public void add(Team teams) {
+
+    }
+
+}
