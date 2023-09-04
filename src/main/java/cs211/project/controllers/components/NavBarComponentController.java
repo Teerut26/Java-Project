520749c3dae2da9ca285a69b885b846f7127adee
Teerut Srithongdee <teerutsr@gmail.com>
2023-09-04 23:54:26 +0700

    [+] add profile image on navbar

--- src/main/java/cs211/project/controllers/components/NavBarComponentController.java
+++ src/main/java/cs211/project/controllers/components/NavBarComponentController.java
@@ -3,6 +3,9 @@ package cs211.project.controllers.components;
 import cs211.project.services.Authentication;
 import cs211.project.services.FXRouter;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
@@ -10,9 +13,12 @@ import java.io.IOException;
 public class NavBarComponentController {
 
     @FXML private Text username;
+    @FXML private Circle userProfileCircle;
 
     @FXML
     public void initialize() {
+        Image img = new Image(getClass().getResource("/cs211/project/assets/image/auth/user.png").toExternalForm());
+        userProfileCircle.setFill(new ImagePattern(img));
         username.setText(Authentication.currentUser.getUserName());
     }
 
