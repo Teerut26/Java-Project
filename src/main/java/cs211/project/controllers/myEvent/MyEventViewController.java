89626872acc4f2334e6b1ef15941dad4630c6256
JarBenjaporn <benjaporn.sukp@ku.th>
2023-09-12 13:43:18 +0700

    not yet MyEventView-Cotroller

--- src/main/java/cs211/project/controllers/myEvent/MyEventViewController.java
+++ src/main/java/cs211/project/controllers/myEvent/MyEventViewController.java
@@ -1,13 +1,22 @@
 package cs211.project.controllers.myEvent;
 
+import cs211.project.controllers.components.EventCardComponentController;
+import cs211.project.models.Event;
 import cs211.project.services.FXRouter;
+import cs211.project.services.datasource.EventFileListDatesource;
+import cs211.project.utils.ComponentLoader;
 import cs211.project.utils.ComponentRegister;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MyEventViewController extends ComponentRegister {
 
@@ -22,6 +31,8 @@ public class MyEventViewController extends ComponentRegister {
     public void initialize() {
         this.loadSideBarComponent(SideBarVBox, "SideBarComponent.fxml");
         this.loadNavBarComponent(NavBarHBox, "NavBarComponent.fxml");
+        eventFileListDatesource = new EventFileListDatesource();
+
     }
 
     @FXML public void goToCreateEvent() {
@@ -31,14 +42,6 @@ public class MyEventViewController extends ComponentRegister {
             throw new RuntimeException(e);
         }
 
-    }
-
-    @FXML public void onSetEventDetailForm() {
-        try {
-            FXRouter.goTo("set-event-detail");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
 }
