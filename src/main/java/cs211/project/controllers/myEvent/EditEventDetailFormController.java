e456db8389b91d678d6a5910c06ffe2a7a0f904e
JarBenjaporn <benjaporn.sukp@ku.th>
2023-09-13 13:57:27 +0700

    Resolved merge conflict in events.csv

--- src/main/java/cs211/project/controllers/myEvent/EditEventDetailFormController.java
+++ src/main/java/cs211/project/controllers/myEvent/EditEventDetailFormController.java
@@ -3,21 +3,41 @@ package cs211.project.controllers.myEvent;
 import cs211.project.services.FXRouter;
 import cs211.project.services.RouteProvider;
 import cs211.project.utils.ComponentRegister;
+import cs211.project.utils.ImageSaver;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.io.File;
+import java.io.IOException;
+
 public class EditEventDetailFormController extends ComponentRegister {
+    @FXML
+    private TextArea TextAreaDescription;
+    @FXML
+    private TextField TextFieldName;
+    @FXML
+    private ImageView addImage;
+    @FXML
+    private DatePicker DataTimeEnd;
+    @FXML
+    private DatePicker DataTimeStart;
+    @FXML
+    private TextField TextFieldQuantity;
     @FXML
     private VBox SideBarVBox;
     @FXML
     private HBox NavBarHBox;
-    private RouteProvider routeProvider;
 
     @FXML
     public void initialize() {
-        routeProvider = (RouteProvider) FXRouter.getData();
-        this.loadSideBarComponent(SideBarVBox, "SideBarComponent.fxml", this.routeProvider);
-        this.loadNavBarComponent(NavBarHBox, "NavBarComponent.fxml", this.routeProvider);
+        this.loadSideBarComponent(SideBarVBox, "SideBarComponent.fxml");
+        this.loadNavBarComponent(NavBarHBox, "NavBarComponent.fxml");
     }
 }
