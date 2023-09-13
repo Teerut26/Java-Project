645c99de51186d93da4abb23fab2d5ad63d30b26
Teerut Srithongdee <teerutsr@gmail.com>
2023-09-14 00:05:04 +0700

    event-detail-page-success

---
+++ src/main/java/cs211/project/services/RouteProvider.java
@@ -0,0 +1,43 @@
+package cs211.project.services;
+
+import cs211.project.models.User;
+
+import java.util.ArrayList;
+
+public class RouteProvider<T> {
+    private T data;
+    private ArrayList<T> dataArrayList;
+    private User userSession;
+
+    public RouteProvider(T data) {
+        this.data = data;
+    }
+
+    public RouteProvider(ArrayList<T> dataArrayList) {
+        this.dataArrayList = dataArrayList;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public ArrayList<T> getDataArrayList() {
+        return dataArrayList;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void setDataArrayList(ArrayList<T> dataArrayList) {
+        this.dataArrayList = dataArrayList;
+    }
+
+    public User getUserSession() {
+        return userSession;
+    }
+
+    public void setUserSession(User userSession) {
+        this.userSession = userSession;
+    }
+}
